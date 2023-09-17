9b773df30d0bb3ad5410019dc8ba00c0b0fa9175
mehediFluttere <mehedii.edu.bd@gmail.com>
2023-09-18 02:58:07 +0600

    10 Light on off

---
+++ swanky_esboo1.ino
@@ -0,0 +1,78 @@
+int pin = 1;
+int pin2=2;
+int pin3=3;
+int pin4=4;
+int pin5=5;
+int pin6=6;
+int pin7=7;
+int pin8=8;
+int pin9=9;
+int pin10=10;
+
+void setup()
+{
+  
+   
+}
+
+void loop()
+{
+  digitalWrite(pin, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin, LOW);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  
+  //2
+    digitalWrite(pin2, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin2, LOW);
+  delay(100.5); 
+  
+  //3
+    digitalWrite(pin3, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin3, LOW);
+  delay(100.5); 
+  
+  //4
+    digitalWrite(pin4, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin4, LOW);
+  delay(100.5); 
+  
+  //5
+    digitalWrite(pin5, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin5, LOW);
+  delay(100.5); 
+  
+  //6 
+    digitalWrite(pin6, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin6, LOW);
+  delay(100.5); 
+  
+  //7
+    digitalWrite(pin7, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin7, LOW);
+  delay(100.5); 
+  
+  //8
+    digitalWrite(pin8, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin8, LOW);
+  delay(100.5); 
+  
+  //9  
+  digitalWrite(pin9, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin9, LOW);
+  delay(100.5); 
+  
+  //10 
+    digitalWrite(pin10, HIGH);
+  delay(100.5); // Wait for 1000 millisecond(s)
+  digitalWrite(pin10, LOW);
+  delay(100.5); 
+}
